8312e74bf311527dd53aa32b38ad7c4fa031dcce
Shohei Aoki <shoaok@gmail.com>
2013-10-19 13:27:13 +0900

    added leap.html to app.js

--- app.js
+++ app.js
@@ -28,8 +28,10 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/', routes.index);
-app.get('/users', user.list);
+//app.get('/', routes.index);
+app.get('/',function(req,res){
+    res.sendfile(__dirname + '/public/javascripts/leap.html');
+});
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
